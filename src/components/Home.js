abdf0fd9c4c9038e9bd9fd6ae8e5ed142c609388
Lekan <jimoridwanlekan@gmail.com>
2022-06-27 15:58:25 +0100

    install react icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,14 @@ import profilepic from '../assets/orange-edit.png';
 
 const Home = () => (
   <div>
-    <div className="flex flex-col text-white bg-[#010105]">
+    <div className="flex flex-col text-white bg-[#010105] md:grid md:grid-cols-2 md:gap-4 m-10 rounded-lg">
       <div>
-        <h1 className="text-3xl m-10 text-left font-bold">
-          Hi, I&apos;m Lekan
+        <h1 className="text-3xl m-10 text-left font-bold md:text-5xl">
+          Hi, I&apos;m Lekan,
           <br />
-          Software Developer
+          Software Developer.
         </h1>
-        <p className="text-base max-w-[95%] text-left m-10">
+        <p className="text-base max-w-[95%] text-left m-10 md:text-xl">
           I&apos;m a software developer specialized in building exceptional
           digital experiences. My focus is on building accessible and human
           centered products.
