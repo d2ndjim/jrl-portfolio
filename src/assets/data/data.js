d1ccf71b6de8ac04051db642dd38212a212fad1a
Lekan <jimoridwanlekan@gmail.com>
2022-07-02 17:47:28 +0100

    fix linters errors

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -2,42 +2,42 @@ const people = [
   {
     id: 1,
     image:
-      "https://user-images.githubusercontent.com/73438276/176234841-bbe8b28a-7d63-451e-bec0-3340902821f9.png",
-    name: "Fortune Stocks",
+      'https://user-images.githubusercontent.com/73438276/176234841-bbe8b28a-7d63-451e-bec0-3340902821f9.png',
+    name: 'Fortune Stocks',
     quote:
-      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
-    repo: "https://github.com/d2ndjim/financial-stocks",
-    live: "https://financial-stocks.vercel.app/",
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+    repo: 'https://github.com/d2ndjim/financial-stocks',
+    live: 'https://financial-stocks.vercel.app/',
   },
   {
     id: 2,
     image:
-      "https://user-images.githubusercontent.com/73438276/176937546-91166ab8-031b-45be-b69f-8488002f2048.png",
-    name: "User Dashboard",
+      'https://user-images.githubusercontent.com/73438276/176937546-91166ab8-031b-45be-b69f-8488002f2048.png',
+    name: 'User Dashboard',
     quote:
-      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
-    repo: "https://github.com/d2ndjim/time-tracking",
-    live: "https://d2ndjim.github.io/time-tracking/",
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+    repo: 'https://github.com/d2ndjim/time-tracking',
+    live: 'https://d2ndjim.github.io/time-tracking/',
   },
   {
     id: 3,
     image:
-      "https://user-images.githubusercontent.com/73438276/176235529-cf97e8b3-f3fe-4031-a1c1-af9536d82019.png",
-    name: "Movie Finder",
+      'https://user-images.githubusercontent.com/73438276/176235529-cf97e8b3-f3fe-4031-a1c1-af9536d82019.png',
+    name: 'Movie Finder',
     quote:
-      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
-    repo: "https://github.com/d2ndjim/Film-Finder",
-    live: "https://d2ndjim.github.io/Film-Finder/",
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+    repo: 'https://github.com/d2ndjim/Film-Finder',
+    live: 'https://d2ndjim.github.io/Film-Finder/',
   },
   {
     id: 4,
     image:
-      "https://user-images.githubusercontent.com/73438276/176701861-1de6d759-7437-47ff-949e-cafeb5cc1c8f.png",
-    name: "Meal Recipe",
+      'https://user-images.githubusercontent.com/73438276/176701861-1de6d759-7437-47ff-949e-cafeb5cc1c8f.png',
+    name: 'Meal Recipe',
     quote:
-      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
-    repo: "https://github.com/d2ndjim/FoodRecipe",
-    live: "https://d2ndjim.github.io/FoodRecipe/",
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+    repo: 'https://github.com/d2ndjim/FoodRecipe',
+    live: 'https://d2ndjim.github.io/FoodRecipe/',
   },
 ];
 
