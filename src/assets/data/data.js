4a5bfe8a98674dba8c921fb4ede4c4341fa61c94
Lekan <jimoridwanlekan@gmail.com>
2022-10-26 15:41:51 +0100

    Modify project thumbnail

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -2,7 +2,7 @@ const projects = [
   {
     id: 1,
     image:
-      'https://user-images.githubusercontent.com/73438276/176234841-bbe8b28a-7d63-451e-bec0-3340902821f9.png',
+      'https://user-images.githubusercontent.com/73438276/198054789-4134dfa3-2e5b-405b-b57c-dc696bffc876.png',
     name: 'Fortune Stocks',
     description:
       'This is a web application that uses react, redux and API to keep track of daily changes and updates in stocks of fortune 500 organizations.',
@@ -12,7 +12,7 @@ const projects = [
   {
     id: 2,
     image:
-      'https://user-images.githubusercontent.com/73438276/193570129-a83eae83-71bb-45d9-912d-8fd312aa0aab.png',
+      'https://user-images.githubusercontent.com/73438276/198051731-4a452233-01e3-4394-88d8-9dded08c1ca2.png',
     name: 'Short Let',
     description:
       'A web application that allows users can make short lets or reservations. Built using React, Redux, Ruby on Rails, Postgresql and BootStrap.',
@@ -22,7 +22,7 @@ const projects = [
   {
     id: 3,
     image:
-      'https://user-images.githubusercontent.com/73438276/193571229-37e3561b-3159-4ac9-ab50-ae7192c2a433.png',
+      'https://user-images.githubusercontent.com/73438276/198055620-7877987b-6156-41a8-aebc-5a8600cd6cbc.png',
     name: 'AES Service',
     description:
       'This is an application that helps users to make electrical engineering or repair appointments. Built using React, Redux and TailwindCSS.',
@@ -32,7 +32,7 @@ const projects = [
   {
     id: 4,
     image:
-      'https://user-images.githubusercontent.com/73438276/176235529-cf97e8b3-f3fe-4031-a1c1-af9536d82019.png',
+      'https://user-images.githubusercontent.com/73438276/198056672-51922c36-135d-4ed3-9512-347fe79a5333.png',
     name: 'Movie Finder',
     description:
       'This Application uses the movie DB API to help users find their favorite genres of movies. Built with HTML, CSS, JavaScript and RestAPI.',
