7d30b86eb74eb405f8fdba22bc5e8b1cc55650c0
Lekan <jimoridwanlekan@gmail.com>
2022-10-26 15:48:33 +0100

    Optimize about section of portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,9 @@ const About = () => (
   <div id="about" className="mt-20 mb-10 bg-[#E8E8FF] pt-4 px-4 pb-0">
     <div className="mt-4 md:mr-10 md:ml-10 md:mt-10 bg-[#F9F9FA] pt-0 px-0 pb-8 rounded-lg">
       <div data-aos="slide-up" className="bg-[#FFFFFF] p-2">
-        <h2 className="text-center font-bold text-4xl text-[#0E0842]">About Me</h2>
+        <h2 className="text-center font-bold text-4xl text-[#0E0842]">
+          About Me
+        </h2>
       </div>
       <div data-aos="slide-up" className="m-10 w-fit">
         <p className="text-[#413C6A] font-bold md:text-base md:leading-8 md:text-center italic">
@@ -13,11 +15,11 @@ const About = () => (
           passion for building accessible and dynamic web applications that
           contribute to great user experiences. I&apos;ve spent the last several
           months in a Remote Environment working with JavaScript, CSS, React,
-          and Redux. I started out in tech doing data science and Machine
-          learning. With the goal of creating special, niche corners of the web,
-          I have been able to collaborate with talented developers from across
-          Africa and Europe while building some unique projects. I&apos;m also a
-          sports fanatic😃.
+          and Redux, Ruby on Rails and PostgreSQL. I started out in tech doing
+          data science and Machine learning. With the goal of creating special,
+          niche corners of the web, I have been able to collaborate with
+          talented developers from across Africa and Europe while building some
+          unique projects. I&apos;m also a sports fanatic😃.
         </p>
       </div>
     </div>
