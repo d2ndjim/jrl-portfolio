34561375128d141887328ab6ca3803696340ebd8
Lekan <jimoridwanlekan@gmail.com>
2023-02-09 17:10:29 +0100

    Update project arrangements

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -2,16 +2,6 @@ const projects = [
   {
     id: 1,
     image:
-      'https://user-images.githubusercontent.com/73438276/198054789-4134dfa3-2e5b-405b-b57c-dc696bffc876.png',
-    name: 'Fortune Stocks',
-    description:
-      'This is a web application that uses react, redux and API to keep track of daily changes and updates in stocks of fortune 500 organizations.',
-    repo: 'https://github.com/d2ndjim/financial-stocks',
-    live: 'https://financial-stocks.vercel.app/',
-  },
-  {
-    id: 2,
-    image:
       'https://user-images.githubusercontent.com/73438276/198051731-4a452233-01e3-4394-88d8-9dded08c1ca2.png',
     name: 'Short Let',
     description:
@@ -20,7 +10,7 @@ const projects = [
     live: 'https://short-let-islands.vercel.app/',
   },
   {
-    id: 3,
+    id: 2,
     image:
       'https://user-images.githubusercontent.com/73438276/198055620-7877987b-6156-41a8-aebc-5a8600cd6cbc.png',
     name: 'AES Service',
@@ -28,6 +18,16 @@ const projects = [
       'This is an application that helps users to make electrical engineering or repair appointments. Built using React, Redux and TailwindCSS.',
     repo: 'https://github.com/d2ndjim/aes-service',
     live: 'https://aes-service.vercel.app/',
+  },
+  {
+    id: 3,
+    image:
+      'https://user-images.githubusercontent.com/73438276/198054789-4134dfa3-2e5b-405b-b57c-dc696bffc876.png',
+    name: 'Fortune Stocks',
+    description:
+      'This is a web application that uses react, redux and API to keep track of daily changes and updates in stocks of fortune 500 organizations.',
+    repo: 'https://github.com/d2ndjim/financial-stocks',
+    live: 'https://financial-stocks.vercel.app/',
   },
   {
     id: 4,
