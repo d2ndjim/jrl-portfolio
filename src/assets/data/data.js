4261d54fe33cbd1ce7a603f8a638c0f7d5e39004
Lekan <jimoridwanlekan@gmail.com>
2023-05-11 13:42:11 +0100

    Update portfolio section

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -12,16 +12,6 @@ const projects = [
   {
     id: 2,
     image:
-      'https://user-images.githubusercontent.com/73438276/233985170-31b2f992-f709-41ef-a2da-2c05c9d1ca3d.png',
-    name: 'Payslice',
-    description:
-      'Payslice make it easy for Businesses and Employees to Access Cash flow On Demand',
-    repo: 'https://github.com/d2ndjim/vaccitracker',
-    live: 'https://payslicehq.com/',
-  },
-  {
-    id: 3,
-    image:
       'https://user-images.githubusercontent.com/73438276/218481851-f1e45eea-6c16-4671-991c-85b9a448c8f7.png',
     name: 'Vaccitracker',
     description:
@@ -30,17 +20,7 @@ const projects = [
     live: 'https://vaccitracker.vercel.app/',
   },
   {
-    id: 4,
-    image:
-      'https://user-images.githubusercontent.com/73438276/198051731-4a452233-01e3-4394-88d8-9dded08c1ca2.png',
-    name: 'Short Let',
-    description:
-      'A web application that allows users can make short lets or reservations. Built using React, Redux, Ruby on Rails, Postgresql and BootStrap.',
-    repo: 'https://github.com/d2ndjim/short-let-frontend',
-    live: 'https://short-let-islands.vercel.app/',
-  },
-  {
-    id: 5,
+    id: 3,
     image:
       'https://user-images.githubusercontent.com/73438276/198055620-7877987b-6156-41a8-aebc-5a8600cd6cbc.png',
     name: 'AES Service',
@@ -48,6 +28,16 @@ const projects = [
       'This is an application that helps users to make electrical engineering or repair appointments. Built using React, Redux and TailwindCSS.',
     repo: 'https://github.com/d2ndjim/aes-service',
     live: 'https://aes-service.vercel.app/',
+  },
+  {
+    id: 4,
+    image:
+      'https://user-images.githubusercontent.com/73438276/234264575-27bd9a24-5fa5-4721-9f0b-3b3a2f7cd53e.png',
+    name: 'Tip calculator',
+    description:
+      'A web application that allows users to calculate tips based on bill percentage. Built using React, TypeScript and TailwindCSS.',
+    repo: 'https://github.com/d2ndjim/short-let-frontend',
+    live: 'https://tip-calculator-d2ndjim.vercel.app/',
   },
 ];
 
