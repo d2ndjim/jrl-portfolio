2f034685b10661df3b550a79c965174f39f6456c
Lekan <jimoridwanlekan@gmail.com>
2023-08-04 11:18:34 +0100

    Add ecommerce store

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,4 +1,5 @@
 import airbnb from '../airbnb-clone.png';
+import store from '../ecomm-store.png';
 
 const projects = [
   {
@@ -14,7 +15,6 @@ const projects = [
   {
     id: 2,
     image: airbnb,
-    // 'https://github-production-user-asset-6210df.s3.amazonaws.com/73438276/252491706-2d7c1911-f4f0-4b41-a5fd-0c76f0693667.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230710%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230710T211448Z&X-Amz-Expires=300&X-Amz-Signature=0872043a50947f12b89dc2668fd8df6105a3a4634da3d5dc8a37eef464ed1df0&X-Amz-SignedHeaders=host&actor_id=73438276&key_id=0&repo_id=508377598',
     name: 'Airbnb NextJs',
     description:
       'An Imitation of the Airbnb website built using the new NextJS 13 app router, TailwindCSS, MongoDB and Prisma.',
@@ -23,6 +23,15 @@ const projects = [
   },
   {
     id: 3,
+    image: store,
+    name: 'Ecommerce Store',
+    description:
+      'This is an Ecommerce store where users can view order items of choice. Built using React, NextJS, and TailwindCSS.',
+    repo: 'https://github.com/d2ndjim/ecommerce-store',
+    live: 'https://ecommerce-store-mu-jade.vercel.app/',
+  },
+  {
+    id: 4,
     image:
       'https://user-images.githubusercontent.com/73438276/198055620-7877987b-6156-41a8-aebc-5a8600cd6cbc.png',
     name: 'AES Service',
@@ -30,16 +39,6 @@ const projects = [
       'This is an application that helps users to make electrical engineering or repair appointments. Built using React, Redux and TailwindCSS.',
     repo: 'https://github.com/d2ndjim/aes-service',
     live: 'https://aes-service.vercel.app/',
-  },
-  {
-    id: 4,
-    image:
-      'https://user-images.githubusercontent.com/73438276/234264575-27bd9a24-5fa5-4721-9f0b-3b3a2f7cd53e.png',
-    name: 'Tip calculator',
-    description:
-      'A web application that allows users to calculate tips based on bill percentage. Built using React, TypeScript and TailwindCSS.',
-    repo: 'https://github.com/d2ndjim/short-let-frontend',
-    live: 'https://tip-calculator-d2ndjim.vercel.app/',
   },
 ];
 
