2e43706004f608e24b3a835faad9e05b5fd02ada
Lekan <jimoridwanlekan@gmail.com>
2023-08-09 14:42:07 +0100

    Add genie AI project

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,29 +1,21 @@
 import airbnb from '../airbnb-clone.png';
 import store from '../ecomm-store.png';
 import dashboard from '../dashboard.png';
+import genie from '../genie.png';
 
 const projects = [
   {
     id: 1,
     image:
-      'https://user-images.githubusercontent.com/73438276/233984769-4076ed6e-b220-4884-b063-3c6354d8c51b.png',
+    'https://user-images.githubusercontent.com/73438276/233984769-4076ed6e-b220-4884-b063-3c6354d8c51b.png',
     name: 'Kwiksell',
     description:
-      'Kwiksell is digital marketplace helps to connect business owners and consumers.',
+    'Kwiksell is digital marketplace helps to connect business owners and consumers.',
     repo: 'https://github.com/d2ndjim?tab=repositories',
     live: 'https://www.kwiksell.com/',
   },
   {
     id: 2,
-    image: airbnb,
-    name: 'Airbnb NextJs',
-    description:
-      'An Imitation of the Airbnb website built using the new NextJS 13 app router, TailwindCSS, MongoDB and Prisma.',
-    repo: 'https://github.com/d2ndjim/airbnb',
-    live: 'https://rent-project-gold.vercel.app/',
-  },
-  {
-    id: 3,
     image: store,
     name: 'Ecommerce Store',
     description:
@@ -32,13 +24,22 @@ const projects = [
     live: 'https://ecommerce-store-mu-jade.vercel.app/',
   },
   {
-    id: 4,
-    image: dashboard,
-    name: 'Ecommerce Dahboard',
+    id: 3,
+    image: genie,
+    name: 'Genie AI',
     description:
-      'This is an Ecommerce Dashboard + CMS application that can be connected to multiple Ecommerce Store applications. Built using NextJS, Prisma,NodeJS, MySQL, and TailwindCSS.',
-    repo: 'https://github.com/d2ndjim/ecommerce-dashboard',
-    live: 'https://ecommerce-dashboard-bice.vercel.app/',
+      'This is a full-stack AI SAAS application. The application allows a user to use AI to do a host of activities such as a chatbot, Image generation for design, Video generation, and Music generation.',
+    repo: 'https://github.com/d2ndjim/ai-saas',
+    live: 'https://genie-ai-eight.vercel.app/',
+  },
+  {
+    id: 4,
+    image: airbnb,
+    name: 'Airbnb NextJs',
+    description:
+    'An Imitation of the Airbnb website built using the new NextJS 13 app router, TailwindCSS, MongoDB and Prisma.',
+    repo: 'https://github.com/d2ndjim/airbnb',
+    live: 'https://rent-project-gold.vercel.app/',
   },
   {
     id: 5,
@@ -49,6 +50,15 @@ const projects = [
       'This is an application that helps users to make electrical engineering or repair appointments. Built using React, Redux and TailwindCSS.',
     repo: 'https://github.com/d2ndjim/aes-service',
     live: 'https://aes-service.vercel.app/',
+  },
+  {
+    id: 6,
+    image: dashboard,
+    name: 'Ecommerce Dahboard',
+    description:
+      'This is an Ecommerce Dashboard + CMS application that can be connected to multiple Ecommerce Store applications. Built using NextJS, Prisma,NodeJS, MySQL, and TailwindCSS.',
+    repo: 'https://github.com/d2ndjim/ecommerce-dashboard',
+    live: 'https://ecommerce-dashboard-bice.vercel.app/',
   },
 ];
 
